ab625fb0d20eeeece8a4f88dbeaceb9f64f23e5b
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-15 15:34:04 +0200

    Added model detection.

--- pinking.py
+++ pinking.py
@@ -7,6 +7,12 @@ import platform
 import subprocess
 import sys
 
+
+# the "Fake GPIO" module
+class FakeGPIO(object):
+    pass
+
+
 # contains a suggestion for installation for various potentially missing
 # module
 PKG_NAMES = {
@@ -16,6 +22,39 @@ PKG_NAMES = {
     3: {
         'RPi.GPIO': 'python3-rpi.gpio',
     }
+}
+
+
+PI_MODELS = {
+    # from http://www.raspberrypi-spy.co.uk/2012/09/
+    #      checking-your-raspberry-pi-board-version/
+    '0002': 'RPi1_B_1.0_256',
+    '0003': 'RPi1_B_1.0_ECN0001_256',
+    '0004': 'RPi1_B_2.0_256',
+    '0005': 'RPi1_B_2.0_256',
+    '0006': 'RPi1_B_2.0_256',
+    '0007': 'RPi1_A_256',
+    '0008': 'RPi1_A_256',
+    '0009': 'RPi1_A_256',
+    '000d': 'RPi1_B_2.0_512',
+    '000e': 'RPi1_B_2.0_512',
+    '000f': 'RPi1_B_2.0_512',
+    '0010': 'RPi1_B+_512',
+    '0011': 'RPi1_Compute_Module_512',
+    '0012': 'RPi1_A+_256',
+    'a01041': 'Rpi2_B_UK_1024',
+    'a21041': 'Rpi2_B_China_1024',
+}
+
+
+def get_cpu_revision():
+    for line in open('/proc/cpuinfo').readlines():
+        if line.startswith('Revision'):
+            return line[line.rfind(':'):].strip()
+
+
+PIN_LAYOUT = {
+
 }
 
 
@@ -108,6 +147,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--fake-gpio', action='store_true',
                         help='Do not use GPIO library, fake input instead.')
+    parser.add_argument('--rev', choices=PI_MODELS.values(),
+                        help='Manually specify hardware revision.')
     args = parser.parse_args()
 
     ensure_uid_0()
@@ -117,9 +158,25 @@ def main():
 
     if not args.fake_gpio:
         required_modules['GPIO'] = 'RPi.GPIO'
+    else:
+        globals()['GPIO'] = FakeGPIO
 
     # import the modules we need
     globals().update(try_imports(required_modules))
+
+    if not args.rev:
+        pi_rev = get_cpu_revision()
+
+        if not pi_rev in PI_MODELS:
+            exiterror('Found the following Revision in /proc/cpuinfo, which '
+                      'is not one I recognize: {}\n'
+                      'You can manually specify one of\n  {}\n\nusing the '
+                      '--rev command line option'
+                      .format(pi_rev, '\n  '.join(
+                          sorted(PI_MODELS.values()))
+                      ))
+    else:
+        pi_rev = args.rev
 
 
 if __name__ == '__main__':
