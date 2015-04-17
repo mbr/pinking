7f50a9210d12b3f21de5b28629fdbc3345efb9c4
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-17 16:12:22 +0200

    Added README.rst and setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pinking',
+    version='0.1.dev1',
+    description='A console-based GPIO GIU for Raspberry Pi. You don\'t need '
+                'it, but you want it!',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/pinking',
+    license='MIT',
+    py_modules=['pinking'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pinking = pinking:main',
+        ],
+    }
+)
