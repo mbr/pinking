c2037a2914f33b348ba051867867cb3b56335c47
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-18 17:21:24 +0200

    UI update (still broken).

--- pinking/ui.py
+++ pinking/ui.py
@@ -40,16 +40,96 @@ class Widget(object):
         return self.scr.getmaxyx()[1]
 
 
-def PinKingUI(Widget):
-    def __init__(self, scr, rev):
+class PinWindow(Widget):
+    def __init__(self, scr, model):
+        self.scr = scr
+        self.model = model
+        model.register_observer(self.redraw)
+        self.redraw()
+
+    def redraw(self):
+        layout = self.model.layout
+        selected = self.model.selected_pin
+        scr = self.scr
+        scr.clear()
+
+        # calculate the maximum width required for any label
+        label_width = max(len(n) for n in layout)
+
+        # label format left and right
+        lfmt = ('{:>%d}' % label_width,
+                '{:<%d}' % label_width)
+        pfmt = '[{:2}]'
+
+        for pin, name in enumerate(layout):
+            row = pin // 2
+            col = pin % 2
+
+            color = curses.color_pair(0)
+            extra_label = color
+            extra_pin = color
+
+            if pin == selected:
+                extra_label = curses.A_BOLD
+                extra_pin = curses.A_BOLD
+
+            # direction
+            pdir = self.model.directions[pin]
+            value = None
+
+            if pdir == GPIO.IN:
+                color = curses.color_pair(6)
+                value = self.model.in_values[pin]
+            elif pdir == GPIO.OUT:
+                color = curses.color_pair(2)
+                value = self.model.out_values[pin]
+
+            # output or input value
+            if value == GPIO.HIGH:
+                extra_label |= curses.A_REVERSE
+                extra_pin |= curses.A_REVERSE
+
+            # special names
+            if name in ('5V', '3V3'):
+                color = curses.color_pair(1)
+            elif name == 'GND':
+                color = curses.color_pair(3)
+
+            # add colors
+            extra_label |= color
+
+            label = lfmt[col].format(layout[pin])
+            scr.addstr(row, col * (label_width + 11), label, extra_label)
+
+            # draw pin:
+            num = pfmt.format(pin + 1)
+            scr.addstr(row, label_width + 1 + col * 5, num, extra_pin)
+
+        scr.refresh()
+
+    @classmethod
+    def from_model(cls, model, y=0, x=0):
+        label_width = max(len(n) for n in model.layout)
+
+        w = 2 * label_width + 12
+        mlen = len(model.layout)
+        h = mlen // 2
+
+        scr = curses.newwin(h, w, y, x)
+
+        return cls(scr, model)
+
+
+class PinKingUI(Widget):
+    def __init__(self, scr, model):
         super(PinKingUI, self).__init__()
 
         self.scr = scr
-        self.rev = rev
+        self.model = model
         self.events = Queue()
 
         # start listening to keyboard events
-        key_thread = threading.Thread(target=self.read_keypresses),
+        key_thread = threading.Thread(target=self._read_keypress)
         key_thread.daemon = True
         key_thread.start()
 
